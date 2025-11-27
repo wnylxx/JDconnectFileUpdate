be75bcb88441dc1e158f4c952ed5049f36a628b7
wnyl_xx <hyy96hyy@naver.com>
2025-11-27 17:43:43 +0900

    command router 생성

---
+++ src/routes/commandRoute.js
@@ -0,0 +1,17 @@
+// Web -> Server -> Rpi 명령 관련 API 주소를 정의
+const express = require('express');
+const router = express.Router();
+const commandController = require('../controllers/commandController');
+
+// POST /api/command/update
+// Body: {project_id, package_id, target_device_ids: [] or ["ALL"]}
+
+router.post('/update', commandController.sendUpdateCommand);
+
+// TODO: 롤백 기능
+
+
+
+
+
+module.exports = router;
